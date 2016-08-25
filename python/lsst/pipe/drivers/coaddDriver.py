03d09785b1ac56f00a48810c6eeb87cfe7d21d8f
Lauren MacArthur <lauren@astro.princeton.edu>
2016-08-25 18:29:49 -0400

    coaddDriver: Default non-calexp persistence to false
    When running coaddDriver to assemble and process coadds, there is
    no need to persist the coadd prior to the "calexp" stage (i.e. at
    the end of DetectCoaddSourcesTask).  Hence the default is set to
    False here, but this can be overridden, e.g. with
    --config assembleCoadd.doWrite=True
    on the command line.

--- python/lsst/pipe/drivers/coaddDriver.py
+++ python/lsst/pipe/drivers/coaddDriver.py
@@ -30,6 +30,7 @@ class CoaddDriverConfig(Config):
         self.makeCoaddTempExp.select.retarget(NullSelectImagesTask)
         self.assembleCoadd.select.retarget(NullSelectImagesTask)
         self.makeCoaddTempExp.doOverwrite = False
+        self.assembleCoadd.doWrite = False
         self.assembleCoadd.doMatchBackgrounds = False
         self.makeCoaddTempExp.bgSubtracted = True
         self.assembleCoadd.badMaskPlanes = ['BAD', 'EDGE', 'SAT', 'INTRP', 'NO_DATA']
