53ebd363cf94b9a2843d85ea15ab874e5ef6153b
Jim Bosch <jbosch@astro.princeton.edu>
2017-12-12 11:29:40 -0500

    Remove INTERP from bad mask planes for coaddition
    Interpolation is pretty good for e.g. cosmic rays, and
    leaving out pixels leads to INEXACT_PSF masks on the coadd,
    which we'd like to minimize.

--- python/lsst/pipe/drivers/coaddDriver.py
+++ python/lsst/pipe/drivers/coaddDriver.py
@@ -43,7 +43,7 @@ class CoaddDriverConfig(Config):
         self.makeCoaddTempExp.doOverwrite = False
         self.assembleCoadd.doWrite = False
         self.assembleCoadd.badMaskPlanes = [
-            'BAD', 'EDGE', 'SAT', 'INTRP', 'NO_DATA']
+            'BAD', 'EDGE', 'SAT', 'NO_DATA']
 
     def validate(self):
         if self.makeCoaddTempExp.coaddName != self.coaddName:
